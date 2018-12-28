5923fd6957f3a02a5555b7f59655519c36febe12
Gary0933 <gary_intel@163.com>
2018-12-28 17:16:53 +0800

    处理服务器返回数据

---
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -0,0 +1,18 @@
+package com.coolweather.android.util;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by b on 12/28/2018.
+ */
+
+public class HttpUtil {
+    //和服务器进行交互
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request =new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
